b3860dd0e19fed02a63faf4752fa71f26ec16fdb
DawnKosmos <martin_hottmann@yahoo.com>
2022-09-25 13:39:23 +0200

    why not wurking

--- backend/exchange/psql/psql.go
+++ backend/exchange/psql/psql.go
@@ -1,29 +1,28 @@
-package exchange
+package psql
 
 import (
 	"context"
-	"github.com/jackc/pgx/v4"
-	"time"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type DB struct {
-	q *pgx.Conn
+	q *pgxpool.Pool
 }
 
 var ctx = context.Background()
 
+/*
 func (d *DB) Ping() error {
 	return d.q.Ping(ctx)
 }
 
-/*
 	BackTestModus:
 	OHCLV(ticker string => "FTX,Btc-perp"
 
 
 
 //Being able to register indexes
-*/
+
 
 func (d *DB) OHCLV(ticker string, resolution int64, start time.Time, end time.Time) ([]Candle, error) {
 	//Separate exchange,ticker => theoretical also having index,ticker
